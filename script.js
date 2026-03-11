cd2b6ab02e6254977e25c3f7493e2523de71ad49
AllenCodess <Aph296@protonmail.com>
2026-03-11 00:17:36 -0400

    successfully can add items to the list

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 // selected content
 const addItemBtn = document.querySelector(".btn");
-
 const itemList = document.querySelector(".items");
 
 addItemBtn.addEventListener("click", function (e) {
   e.preventDefault();
   const inputValue = document.querySelector(".form-input").value;
-  console.log(inputValue);
+  createListItems(inputValue);
 });
+
+function createListItems(inputValue) {
+  const li = document.createElement("li");
+  li.textContent = inputValue;
+  itemList.appendChild(li);
+}
